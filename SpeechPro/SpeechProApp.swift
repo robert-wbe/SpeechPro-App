e18545ccc5b5abccfa85612b2efbd2c696761a9e
Robert Wiebe <robert.wiebe9@icloud.com>
2022-10-06 17:43:15 +0200

    Initial Commit

---
+++ SpeechPro/SpeechProApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechProApp.swift
+//  SpeechPro
+//
+//  Created by Robert Wiebe on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SpeechProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
